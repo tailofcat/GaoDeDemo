489a00862ea8d554a2471cea153222a612ca42ab
tailofcat <tailofcat@users.noreply.github.com>
2026-05-31 04:40:18 +0000

    feat: 创建高德地图API V2最小系统
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- amap-minimal/server.js
+++ amap-minimal/server.js
@@ -47,16 +47,12 @@ async function proxyRequest(req, res, targetBase, stripPrefix, extraParams) {
   }
 }
 
-app.all('/amap/api/*', (req, res) => {
-  proxyRequest(req, res, 'https://webapi.amap.com', '/amap/api', { key: AMAP_KEY });
-});
-
-app.all('/_AMapService/v4/map/styles/*', (req, res) => {
-  proxyRequest(req, res, 'https://webapi.amap.com', '/_AMapService/v4/map/styles', { jscode: AMAP_SECURITY_KEY });
+app.all('/_AMapService/v4/map/styles', (req, res) => {
+  proxyRequest(req, res, 'https://webapi.amap.com', '/v4/map/styles', { jscode: AMAP_SECURITY_KEY });
 });
 
 app.all('/_AMapService/*', (req, res) => {
-  proxyRequest(req, res, 'https://restapi.amap.com', '/_AMapService', { jscode: AMAP_SECURITY_KEY });
+  proxyRequest(req, res, 'https://restapi.amap.com', '/', { jscode: AMAP_SECURITY_KEY });
 });
 
 app.listen(PORT, () => {
